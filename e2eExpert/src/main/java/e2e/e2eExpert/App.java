c09e1444a3e2ffcb7aedda56cc0d8c5ce41f1733
Antonio Seraus <antonio.seraus@gmail.com>
2019-01-24 23:33:20 +0100

    first commit e2eReports

---
+++ e2eExpert/src/main/java/e2e/e2eExpert/App.java
@@ -0,0 +1,13 @@
+package e2e.e2eExpert;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ e2eExpert/src/test/java/com/e2e/testcases/TC003.java
@@ -0,0 +1,13 @@
+package com.e2e.testcases;
+
+import org.testng.annotations.Test;
+
+import com.e2e.base.Testbase;
+
+public class TC003 extends Testbase {
+
+  @Test
+  public void f() {
+	  System.out.println(this.getClass().getName());
+  }
+}
